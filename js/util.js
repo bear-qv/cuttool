2b3e6e1558448bb1a990556aa425e9a624b400d0
qovciu <372903575@qq.com>
2015-03-11 08:14:21 +0800

    界面修改
    上传后的界面进行了优化。

--- js/util.js
+++ js/util.js
@@ -1,5 +1,5 @@
 /**
- *
+ * Util.js
  * @authors Qv(foru.fy@gmail.com)
  * @date    2015-02-09 10:19:19
  * @version 0.1
@@ -13,9 +13,33 @@
  * return  array[obj]
  */
 function $(type) {
-	return document.querySelectorAll(type);
+    return document.querySelectorAll(type);
 };
 
+/**
+ * style , 返回一个结点对联调整样式函数
+ *
+ * 参数 : name , value
+ * return  function
+ */
+
+function style(name, value) {
+    return function () {
+        for (var i = 0, t; t = this[i]; i++) {
+            t.style[name] = value;
+        }
+    }
+}
+
+/**
+ * show/hide 让结点队列隐藏/SHOW
+ *
+ * 参数 : NULL
+ * return  NULL
+ */
+
+HTMLCollection.prototype.hide = NodeList.prototype.hide = style('display', 'none');
+HTMLCollection.prototype.show = NodeList.prototype.show = style('display', 'block');
 
 /**
  * log : console.log
@@ -23,7 +47,7 @@ function $(type) {
  * 提高效率
  */
 function log(s) {
-	console.log(s);
+    console.log(s);
 }
 
 /**
@@ -32,9 +56,8 @@ function log(s) {
  * 提高效率
  */
 function Int(s) {
-	return parseInt(s, 10);
+    return parseInt(s, 10);
 }
-
 
 
 /**
@@ -43,11 +66,11 @@ function Int(s) {
  * 给原生对象添加方法
  */
 document.on = Element.prototype.on = Element.prototype.addEventListener;
-NodeList.prototype.on = function(event, fn) {
-	[]['forEach'].call(this, function(el) {
-		el.on(event, fn);
-	});
-	return this;
+NodeList.prototype.on = function (event, fn) {
+    []['forEach'].call(this, function (el) {
+        el.on(event, fn);
+    });
+    return this;
 };
 document.off = Element.prototype.off = Element.prototype.removeEventListener;
 
@@ -58,12 +81,12 @@ document.off = Element.prototype.off = Element.prototype.removeEventListener;
  * return null
  */
 function error(msg) {
-	throw new Error(msg);
+    throw new Error(msg);
 };
 
 /**
  * 关于~特殊用法
- * 按位非运算符  
+ * 按位非运算符
  * 2015/3/2 : 简单的理解就是改变运算数的符号并减去1
  *
  *  ~[1,2,3,4,5].indexOf(9) 在这个中，返回值就可以直接if了
